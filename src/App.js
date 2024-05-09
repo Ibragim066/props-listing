2750e2ab8f8c568bcfa8b75ca44b51bbf37ed118
Ibragim <gamemega.ao@gmail.com>
2024-05-09 09:51:14 +0300

    implemented logic

--- src/App.js
+++ src/App.js
@@ -2,3 +2,7 @@ import "./App.css";
 import { Listing } from "./components/Listing";
 import etsy from "./data/etsy.json";
 import { processEtsyItems } from "./utils";
+
+const App = () => <Listing items={processEtsyItems(etsy)} />;
+
+export default App;
